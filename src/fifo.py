07ce8e60d49abc53782707a7ff6625cbdb2cb14d
hiralshukla <hiralshukla365@gmail.com>
2026-03-01 11:01:23 -0500

    feat: implement fifo, fix main.py

--- src/fifo.py
+++ src/fifo.py
@@ -1,6 +1,26 @@
+from collections import deque
 
 # fifo = first in first out
 # evict the item that has been in the cache the longest
 
 def fifo_policy(k, m, r): 
-    print("fifo")
+    
+    the_cache = set()
+    queue = deque()
+    num_misses = 0
+
+
+    for req in r:
+        if req in the_cache: 
+            continue
+
+        num_misses += 1
+
+        if len(the_cache) == k:
+            old = queue.popleft()
+            the_cache.remove(old)
+            
+        the_cache.add(req)
+        queue.append(req)
+
+    return num_misses
