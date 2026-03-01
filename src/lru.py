4553cce1383a20ace04445cde21c1d0a3d942e87
hiralshukla <hiralshukla365@gmail.com>
2026-03-01 11:15:13 -0500

    feat: implement lru

--- src/lru.py
+++ src/lru.py
@@ -1,6 +1,22 @@
+from collections import OrderedDict
 
 # lru = least recently used
 # evict the item whose most recent access time is the oldest
 
 def lru_policy(k, m, r): 
-    print("lru")
+    
+    the_cache = OrderedDict()
+    num_misses = 0
+    
+
+    for req in r: 
+        
+        if req in the_cache: 
+            the_cache.move_to_end(req)
+        else: 
+            num_misses += 1
+            if len(the_cache) == k:
+                the_cache.popitem(last=False)
+            the_cache[req] = True
+
+    return num_misses
